719406850ab3867f135f9e34e73ed39f61e38d1b
Wei Hu <weihu@cs.virginia.edu>
2010-11-28 05:04:10 -0500

    Implemented printCode

--- lib/p1/printCode.cpp
+++ lib/p1/printCode.cpp
@@ -20,31 +20,63 @@
 #include "llvm/Instruction.h"
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/ADT/Statistic.h"
-#include <iostream>
-#include <vector>
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/InstIterator.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/User.h"
 using namespace llvm;
 
 namespace {
   class printCode : public FunctionPass {
+  private:
+    DenseMap<const Value*, int> instMap;
+
     public:
     static char ID; // Pass identification, replacement for typeid
     printCode() : FunctionPass(&ID) {}
+
+    void addToMap(Function &F) {
+      static int id = 1;
+      for (inst_iterator i = inst_begin(F), E = inst_end(F); i != E; ++i, ++id)
+        // Convert the iterator to a pointer, and insert the pair
+        instMap.insert(std::make_pair(&*i, id));
+    }
     
     //**********************************************************************
     // runOnFunction
     //**********************************************************************
     virtual bool runOnFunction(Function &F) {
       // print fn name
-      std::cerr << "FUNCTION " << F.getNameStr() << "\n";
+      errs() << "FUNCTION " << F.getName() << "\n";
 
-      // MISSING: Add code here to do the following:
       //          1. Iterate over the instructions in F, creating a
       //             map from instruction address to unique integer.
-      //             (It is probably a good idea to put this code in
-      //             a separate, private method.)
+      addToMap(F);
+
       //          2. Iterate over the basic blocks in the function and
       //             print each instruction in that block using the format
       //             given in the assignment.
+      for (Function::iterator b = F.begin(), e = F.end(); b != e; ++b) {
+        errs() << "\nBASIC BLOCK " << b->getName() << "\n";
+        for (BasicBlock::iterator i = b->begin(), e = b->end(); i != e; ++i) {
+          int id = instMap.lookup(&*i);
+          errs() << "%" << id << ":\t" << i->getOpcodeName() << "\t";
+          unsigned n = i->getNumOperands();
+          for (unsigned j = 0; j < n; j++) {
+            Value *v = i->getOperand(j);
+            if (isa<Instruction>(v)) {
+              Instruction *op = cast<Instruction>(v);
+              errs() << "%" << instMap.lookup(op);
+            }
+            else if (v->hasName())
+              errs() << v->getName();
+            else
+              errs() << "XXX";
+            errs() << " ";
+          }
+          errs() << "\n";
+        }
+      }
 
       return false;  // because we have NOT changed this function
     }
